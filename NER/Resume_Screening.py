98797d70baf697a210f8882cdf7cc79cab55897f
AmritaNeogi <amritaneogi0111@gmail.com>
2023-05-07 05:51:29 -0700

    updating links

--- NER/Resume_Screening.py
+++ NER/Resume_Screening.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+###########################################################
 # # Resume Screening using spaCy
-
+###########################################################
 # This project involves using spacy to perform entity recognition on 200 resumes and exploring different NLP tools for text analysis. The goal is to assist recruiters in quickly sifting through a large number of job applications. To aid hiring managers in determining whether to proceed to the interview stage, a skills match feature has been added, which uses a metric. Two datasets will be used, one containing resume texts and the other containing skills that will be used to create an entity ruler.
 
+
+###########################################################
 # # Imports
-
-# In[1]:
-
+###########################################################
 
 #Data loading/ Data manipulation
 import pandas as pd
@@ -39,10 +40,7 @@ from spacy import displacy
 import warnings
 warnings.filterwarnings("ignore")
 
-
 # # Loading Data
-
-# In[2]:
 
 
 df = pd.read_csv("/Resume/Resume.csv")
@@ -55,15 +53,12 @@ data.head()
 
 # # Loading spaCy Model
 
-# In[3]:
-
-
 nlp = spacy.load("en_core_web_lg")
 skill_pattern_path = "jz_skill_patterns.jsonl"
 
 # # Entity Ruler
 
-# In[4]:
+
 
 
 ruler = nlp.add_pipe("entity_ruler")
@@ -93,14 +88,10 @@ def unique_skills(x):
 
 # # Resume Text Cleaning
 
-# In[6]:
-
 
 nltk.download(['stopwords','wordnet'])
-nltk.download('omw-1.4')
+_ = nltk.download('omw-1.4')
 
-
-# In[7]:
 
 
 clean = []
@@ -124,16 +115,12 @@ for i in range(data.shape[0]):
 
 # # Implementing the Functions
 
-# In[8]:
 
 
 data["Clean_Resume"] = clean
 data["skills"] = data["Clean_Resume"].str.lower().apply(get_skills)
 data["skills"] = data["skills"].apply(unique_skills)
 data.head()
-
-
-# In[10]:
 
 
 # set the random seed
@@ -146,15 +133,10 @@ fig.update_xaxes(categoryorder="total descending")
 
 fig.show()
 
-
-# In[11]:
-
+# from the fig. we can asses what skills a candidate needs to increase the probability of getting hired
 
 Job_cat = data["Category"].unique()
 Job_cat = np.append(Job_cat, "ALL")
-
-
-# In[12]:
 
 
 # We want to know the skills required for someone apply for a BUSINESS-DEVELOPMENT job
@@ -183,33 +165,21 @@ fig.show()
 
 # # Entity Recognition
 
-# In[14]:
-
-
 sent = nlp(data["Resume_str"].iloc[0])
 displacy.render(sent, style="ent", jupyter=True)
 
 
 # # Dependency Parsing
 
-# In[15]:
-
-
 displacy.render(sent[0:10], style="dep", jupyter=True, options={"distance": 90})
 
 
 # # Custom Entity Recognition
 
-# In[16]:
-
 
 patterns = df.Category.unique()
 for a in patterns:
     ruler.add_patterns([{"label": "Job-Category", "pattern": a}])
-
-
-# In[17]:
-
 
 # options=[{"ents": "Job-Category", "colors": "#ff3232"},{"ents": "SKILL", "colors": "#56c426"}]
 colors = {
@@ -242,14 +212,8 @@ displacy.render(sent, style="ent", jupyter=True, options=options)
 
 # # Resume Anlaysis
 
-# In[18]:
-
-
 # taking example of a random resume 
 input_resume= "Jonathan Andrews Data Scientist I am a certified data scientist professional, who loves building machine learning models and blogs about the latest AI technologies. I am currently testing AI Products at PEC-PITC, which later gets approved for human trials. jandrews@email.com +112345678 Phoenix, Arizona abidaliawan.me WORK EXPERIENCE Data Scientist Arizona Innovation and Testing Center - PEC 04/2021 - Present, Phoenix, Arizona Redesigned data of engineers that were mostly scattered and unavailable. Designed dashboard and data analysis report to help higher management make better decisions. Accessibility of key information has created a new culture of making data-driven decisions. Contact: Rameez Ali - rameezali@email.com Data Scientist Freelancing/Kaggle 11/2020 - Present, Phoenix, Arizona Engineered a healthcare system. Used machine learning to detect some of the common decisions. The project has paved the way for others to use new techniques to get better results. Participated in Kaggle machine learning competitions. Learned new techniques to get a better score and finally got to 1 percent rank. Researcher / Event Organizer CREDIT 02/2017 - 07/2017, Kuala Lumpur, Malaysia Marketing for newly build research lab. Organized technical events and successfully invited the multiple company's CEO for talks. Reduced the gap between industries and educational institutes. Research on new development in the IoT sector. Created research proposal for funding. Investigated the new communication protocol for IoT devices. Contact: Dr. Tan Chye Cheah - dr.chyecheah.t@apu.edu.my EDUCATION MSc in Technology Management Staffordshire University 11/2015 - 04/2017, Postgraduate with Distinction Challenges in Implementing IoT-enabled Smart cities in Malaysia. Bachelors Electrical Telecommunication Engineering COMSATS Institute of Information Technology, Phoenix 08/2010 - 01/2014, CGPA: 4.00 Networking Satellite communications Programming/ Matlab Telecommunication Engineering SKILLS Designing Leadership Media/Marketing R/Python SQL Tableau NLP Data Analysis Machine learning Deep learning Webapp/Cloud Feature Engineering Ensembling Time Series Technology Management ACHIEVEMENTS 98th Hungry Geese Simulation Competition (08/2021) 2nd in Covid-19 vaccinations around the world (07/2021) 8th in Automatic Speech Recognition in WOLOF (06/2021) Top 10 in WiDS Datathon. (03/2021) 40th / 622 in MagNet: Model the Geomagnetic Field Hosted by NOAA (02/2021) 18th in Rock, Paper, Scissors/Designing AI Agent Competition. (02/2021) PROJECTS Goodreads Profile Analysis WebApp (09/2021) Data Analysis Web Scraping XLM Interactive Visualization Contributed in orchest.io (08/2021) Testing and Debuging Technical Article Proposing new was to Improve ML pipelines World Vaccine Update System (06/2021) Used sqlite3 for database Automated system for daily update the Kaggle DB and Analysis Interactive dashboard mRNA-Vaccine-Degradation-Prediction (06/2021) Explore our dataset and then preprocessed sequence, structure, and predicted loop type features Train deep learning GRU model Trip Advisor Data Analysis/ML (04/2021) Preprocessing Data, Exploratory Data analysis, Word clouds. Feature Engineering, Text processing. BiLSTM Model for predicting rating, evaluation, model performance. Jane Street Market Prediction (03/2021) EDA, Feature Engineering, experimenting with hyperparameters. Ensembling: Resnet, NN Embeddings, TF Simple NN model. Using simple MLP pytorch model. Achievements/Tasks Achievements/Tasks Achievements/Tasks Thesis Courses"
-
-
-# In[19]:
 
 
 sent2 = nlp(input_resume)
@@ -258,14 +222,7 @@ displacy.render(sent2, style="ent", jupyter=True, options=options)
 
 # # Accuracy
 
-# In[20]:
-
-
 input_skills = "Database, SQL, Machine Learning" # example of input skills
-
-
-# In[21]:
-
 
 req_skills = input_skills.lower().split(",")
 resume_skills = unique_skills(get_skills(input_resume.lower()))
@@ -281,13 +238,8 @@ print(f"The current Resume is {match}% matched to your requirements")
 
 # We can also see the skills mentioned in the input resume.
 
-# In[23]:
-
 
 print(resume_skills)
-
-
-# In[ ]:
 
 
 
